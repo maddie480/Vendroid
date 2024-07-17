95712df47a842bab6771a52d5883d90718824b77
Maddie <52103563+maddie480@users.noreply.github.com>
2024-07-17 19:27:40 +0200

    Replace Vencord sources

--- app/src/main/java/dev/vendicated/vencord/Constants.java
+++ app/src/main/java/dev/vendicated/vencord/Constants.java
@@ -1,5 +1,5 @@
 package dev.vendicated.vencord;
 
 public class Constants {
-    public static final String JS_BUNDLE_URL = "https://github.com/Vendicated/Vencord/releases/download/devbuild/browser.js";
+    public static final String JS_BUNDLE_URL = "https://raw.githubusercontent.com/maddie480/Vendroid/main/dist/browser.js";
 }
